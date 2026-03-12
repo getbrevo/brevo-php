49268ac059d5d3dc0e70ff07847bb13bb91eab2c
Mauricio <mourraille@me.com>
2026-03-12 10:34:48 +0100

    fix: use RFC3339_EXTENDED for datetime serialization to match WireMock stubs

--- src/Core/Types/Constant.php
+++ src/Core/Types/Constant.php
@@ -8,5 +8,5 @@ class Constant
 {
     public const DateFormat = 'Y-m-d';
     public const DateDeserializationFormat = "!" . self::DateFormat;
-    public const DateTimeFormat = DateTimeInterface::RFC3339;
+    public const DateTimeFormat = DateTimeInterface::RFC3339_EXTENDED;
 }
